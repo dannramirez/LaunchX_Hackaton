72bc580f707470f4696fe04cf4356b5ca0a63d32
Daniel Ramirez <contact@dann.dev>
2022-05-14 16:53:54 -0500

    add Test: Prueba para verificar que la funcion de iniciar sesion devuelve el id de usuario y un token de acceso al ingresar credenciales validas

--- Back-End/test/lib/controllers/user.test.js
+++ Back-End/test/lib/controllers/user.test.js
@@ -2,32 +2,55 @@ require("dotenv").config(); // this is important!
 const db = require("../../../lib/relations");
 const config = require("../../utils/configTest");
 
-const user = {
-    name: "Prueba1",
-    username: "Oceanauta1",
-    email: "test@te1st.com",
-    password: "prueba",
-    role: ["admin"]
-};
+let services;
 
+beforeAll(async () => {
+    if (!services) {
+        try {
+            services = await db(config.db);
+        } catch (e) {
+            console.log(e);
+        }
+    }   
+});
 
 describe("Test para userController", () => {
-    test("Test para crear un usuario en la base de datos", async () => {
-        let services;
-        if (!services) {
-            try {
-                services = await db(config.db);
-            } catch (e) {
-                console.log(e);
-            }
-        }
+    const user = {
+        name: "Prueba1",
+        username: "Oceanauta1",
+        email: "test@te1st.com",
+        password: "prueba",
+        role: ["admin"]
+    };
 
+    test("Test para crear un usuario en la base de datos", async () => {
+       
         const User = services.User;
 
         try {
             const response = await User.createUser(user);
             expect(response).toStrictEqual({"message": "User was registered successfully!"});
             
+        } catch (e) {
+            console.log(e);
+            return e;
+        }
+    });
+
+    test("Test para crear Iniciar Sesión con la base de datos", async () => {
+        const user = {
+            name: "Prueba1",
+            username: "Oceanauta1",
+            email: "test@te1st.com",
+            password: "prueba",
+            role: ["admin"]
+        };
+
+        const User = services.User;
+
+        try {
+            const response = await User.signin(user);
+            expect(response.auth).toBe(true);         
         } catch (e) {
             console.log(e);
             return e;
