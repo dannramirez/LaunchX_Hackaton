c8601617b786b327a772c05f8472723f478bf0e2
Daniel Ramirez <contact@dann.dev>
2022-05-14 06:53:43 -0500

    add: Estructura servidor express

---
+++ Back-End/lib/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/api/", (request, response) => {
+    response.json({
+        message: "Oceanautas API -- Hackathon"
+    });
+});
+
+const server = app.listen(port, () => {
+    console.log(`Server is Listening on ${port}`);
+});
+
+module.exports = {
+    app,
+    server
+};
