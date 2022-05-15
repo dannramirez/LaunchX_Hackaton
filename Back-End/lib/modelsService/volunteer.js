c37a973f581a4114c2d81bb3750ee0d1798c82ac
Daniel Ramirez <contact@dann.dev>
2022-05-14 22:01:31 -0500

    add: Metodos para crear nuevos voluntarios, para obtener una lista donde un usuario es voluntario, y una lista donde muestra a los usuarios voluntarios

--- Back-End/lib/modelsService/volunteer.js
+++ Back-End/lib/modelsService/volunteer.js
@@ -1,7 +1,24 @@
 function setupVolunteer(VolunteerModel, UserModel, ReportModel) {
     
+    async function createVolunteer(volunteer) {
+        try {
+            await VolunteerModel.create(volunteer);
+            return ({
+                message: "Voluntario registrado exitosamente!"
+            });
+        } catch (error) {
+            return ({
+                message: "Voluntario no registrado!"
+            });
+        }
+    }
+
     function findById(id) {
-        return VolunteerModel.findById(id);
+        return VolunteerModel.findOne({
+            where: {
+                id: id
+            }
+        });
     }
 
     function findAll() {
@@ -10,8 +27,35 @@ function setupVolunteer(VolunteerModel, UserModel, ReportModel) {
         });
     }
 
+    function findByUserId(id) {
+        return VolunteerModel.findAll({
+            where: {
+                userId: id
+            },
+            include: [{
+                model: ReportModel
+            }, ],
+            raw: true
+        });
+    }
+
+    function findByReportId(id) {
+        return VolunteerModel.findAll({
+            where: {
+                reportId: id
+            },
+            include: [{
+                model: UserModel
+            }, ],
+            raw: true
+        });
+    }
+
     return {
         findById,
+        createVolunteer,
+        findByUserId,
+        findByReportId,
         findAll
     };
 }
