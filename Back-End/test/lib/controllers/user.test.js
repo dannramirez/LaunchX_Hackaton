f7a8967a287d1163605797139406ed787d0589d4
Daniel Ramirez <contact@dann.dev>
2022-05-14 21:12:32 -0500

    add Tests: Test para probar los metodos de los donativos

--- Back-End/test/lib/controllers/user.test.js
+++ Back-End/test/lib/controllers/user.test.js
@@ -101,17 +101,16 @@ describe("Test para userController", () => {
         expect(usuario.id).toBe(response[0].id);
     });
 
-
 });
 
 describe("Test para reportController", () => {
 
-    test("Test para crear un usuario en la base de datos", async () => {
-        
+    test("Test para crear un reporte en la base de datos", async () => {
+
         const Report = services.Report;
         const User = services.User;
         const response = await User.findAll();
-        
+
         const report = {
             name: "Derrame Pacifico",
             description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -121,6 +120,83 @@ describe("Test para reportController", () => {
             userId: response[0].id
         };
         const responseReport = await Report.createReport(report);
-        expect(responseReport).toStrictEqual({ "message": "Report registered successfully!"});
+        expect(responseReport).toStrictEqual({
+            "message": "Report registered successfully!"
+        });
     });
+
+    test("Test para devolver todos los reportes de la base de datos", async () => {
+        const Report = services.Report;
+        const response = await Report.findAll();
+        expect(response[0].name).toBe("Derrame Pacifico");
+    });
+
+    test("Test para devolver un usuario a partir de su ID", async () => {
+        const Report = services.Report;
+        const response = await Report.findAll();
+
+        const reporte = await Report.findById(response[0].id);
+        expect(reporte.id).toBe(response[0].id);
+    });
+});
+
+describe("Test para donationController", () => {
+
+    test("Test para crear un donativo en la base de datos", async () => {
+
+        const Donation = services.Donation;
+        const Report = services.Report;
+        const User = services.User;
+        const reporte = await Report.findAll();
+        const usuario = await User.findAll();
+
+        const donation = {
+            quantity: 500,
+            reportId: reporte[0].id,
+            userId: usuario[0].id
+        };
+        const donation2 = {
+            quantity: 501,
+            reportId: reporte[0].id,
+            userId: usuario[0].id
+        };
+        await Donation.createDonation(donation2);
+        const response = await Donation.createDonation(donation);
+        expect(response).toStrictEqual({
+            message: "Donación registrada exitosamente!"
+        });
+    });
+
+    test("Test para devolver todos los donativos de la base de datos", async () => {
+        const Donation = services.Donation;
+        const response = await Donation.findAll();
+        expect(response[0].quantity).toBe("501");
+    });
+
+    test("Test para devolver una donación a partir del ID", async () => {
+        const Donation = services.Report;
+        const response = await Donation.findAll();
+
+        const donativo = await Donation.findById(response[0].id);
+        expect(donativo.id).toBe(response[0].id);
+    });
+
+    test("Test para devolver todos los donativos a partir del ID de un reporte", async () => {
+        const Donation = services.Donation;
+        const Report = services.Report;
+        const reporte = await Report.findAll();
+        const donativo = await Donation.findByReportId(reporte[0].id);
+        expect(donativo[0].reportId).toBe(reporte[0].id);
+    });
+
+    test("Test para devolver todos los donativos a partir del ID de un reporte", async () => {
+        const Donation = services.Donation;
+        const User = services.User;
+        const usuario = await User.findAll();
+        const donativo = await Donation.findByUserId(usuario[0].id);
+        console.log(donativo);
+        expect(donativo[0].userId).toBe(usuario[0].id);
+    });
+    
+
 });
